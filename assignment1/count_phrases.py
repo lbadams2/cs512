f78fe6af942d49193ac983de79b75927b332e1ae
Liam Adams <lbadams2@ncsu.edu>
2019-03-01 23:49:59 -0600

    report

---
+++ assignment1/count_phrases.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+e = ET.parse('/Users/liamadams/Documents/school/AutoPhrase/models/YELP/segmentation.txt').getroot()
+#e = ET.parse('test_seg.txt').getroot()
+multi_set = set()
+single_set = set()
+for aphrase in e.findall('phrase'):
+    phrase_text = aphrase.text
+    if ' ' in phrase_text:
+        multi_set.add(phrase_text)
+    else:
+        single_set.add(phrase_text)
+
+print('Number of unique single word phrases ' + str(len(single_set)))
+print('Number of unique multi word phrases ' + str(len(multi_set)))
+
+# add root
+# replace &, <<<, <<, !</, <.IO62-CNI, <.MIAPJ0000PUS.>, <a
