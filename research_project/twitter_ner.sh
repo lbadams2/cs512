b1aaa03a7e8119db686ab071a1e23799d4c1aaba
Liam Adams <lbadams2@ncsu.edu>
2019-03-20 22:42:25 -0500

    update twitter ner script

--- research_project/twitter_ner.sh
+++ research_project/twitter_ner.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-rm -f toy_train
+TRAIN_FILE='train.json'
+PROCESSED_FILE='train_processed'
+rm -f $PROCESSED_FILE
 #tweet="$(jq '.[3][0]' toy.json | awk '{gsub("\"","");print}')"
-c=0
-while [ 'null' != "$(jq ".[$c][0]" toy.json)" ]; do
-    c=$c+1
-    tweet=$(jq ".[$c][0]" toy.json)
+json_count=0
+while [ 'null' != "$(jq ".[$json_count][0]" $TRAIN_FILE)" ]; do    
+    tweet="$(jq ".[$json_count][0]" $TRAIN_FILE)"
     tweet="${tweet:1}"
     tweet=${tweet::${#tweet}-3}
     tweet="$tweet "
     #tweet=$(echo $tweet | sed -E 's/[[:punct:]]/ /g')
     echo $tweet
-    tags=$(echo $(jq ".[$c][1]" toy.json) | awk '{gsub(",|\\[|\\]|\"","");print}')
+    tags=$(echo $(jq ".[$json_count][1]" $TRAIN_FILE) | awk '{gsub(",|\\[|\\]|\"","");print}')
+    json_count=$(($json_count+1))
     tag_array=($tags)
     word=''
     prev_tag=''
     last_char_space=''
     multi_word_entity=''
-
+    char_count=0
     for (( i=0; i<${#tweet}; i++ )); do
-        current_char=${tweet:$i:1}
-        if [[ $current_char == *['!'.,?:\;\"\\]* ]]; then # maybe add \# and \@
+        current_char=${tweet:$i:1}        
+        if [[ $current_char == *['!'.“”,?:\[\]\(\)\;\"\\]* ]]; then # maybe add \# and \@
+            #echo "skip $current_char previous tag is $prev_tag"
+            if [[ $current_char != \\ ]]; then
+                char_count=$(($char_count+1))
+            fi
             continue
         fi
-        current_tag=${tag_array[$i]}
+        current_tag=${tag_array[$char_count]}
+        #echo "$current_char $char_count $current_tag"
+        char_count=$(($char_count+1))
 
-        if [[ $current_tag = I* && $last_char_space = 'yes' ]]; then
-            echo "Multi word entity $word $current_char"
+        if [[ ( $current_tag = I* || $current_tag = E* ) && $last_char_space = 'yes' ]]; then
+            #echo "Multi word entity $word $current_char"
             last_char_space=''
             multi_word_entity='yes'
         fi
@@ -47,11 +55,13 @@ while [ 'null' != "$(jq ".[$c][0]" toy.json)" ]; do
                     str="$word $tag"
                 fi            
             fi
-            echo $str >> toy_train
+            if [[ $word = *[!\ ]* ]]; then
+                echo $str >> $PROCESSED_FILE
+            fi
             word=''
             last_char_space='yes'
         fi
         prev_tag=$current_tag
     done
-    echo '' >> toy_train
+    echo '' >> $PROCESSED_FILE
 done
