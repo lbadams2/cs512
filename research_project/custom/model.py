139b0e5b910342874c267a1b1056a68ceac46fa4
Liam Adams <lbadams2@ncsu.edu>
2019-05-06 15:31:44 -0500

    running no errors

--- research_project/custom/model.py
+++ research_project/custom/model.py
@@ -9,22 +9,25 @@ from tqdm import trange
 import logging
 
 
-total_word_to_idx = {}
-word_to_idx = {}
+#total_word_to_idx = {}
 tag_to_idx = {}
+
+TRAIN_PATH = 'research_project/nltk/train.txt'
+TEST_PATH = 'research_project/nltk/test.txt'
+DEV_PATH = 'research_project/nltk/dev.txt'
 
 class Net(nn.Module):
     def __init__(self, params):
         super(Net, self).__init__()
 
         # maps each token to an embedding_dim vector
-        self.embedding = nn.Embedding(params.vocab_size, params.embedding_dim)
+        self.embedding = nn.Embedding(params['vocab_size'], params['embedding_dim'])
 
         # the LSTM takens embedded sentence
-        self.lstm = nn.LSTM(params.embedding_dim, params.lstm_hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(params['embedding_dim'], params['lstm_hidden_dim'], batch_first=True)
 
         # fc layer transforms the output to give the final output layer
-        self.fc = nn.Linear(params.lstm_hidden_dim, params.number_of_tags)
+        self.fc = nn.Linear(params['lstm_hidden_dim'], params['number_of_tags'])
     
     def forward(self, s):
         # apply the embedding layer that maps each token to its embedding
@@ -34,7 +37,7 @@ class Net(nn.Module):
         s, _ = self.lstm(s)     # dim: batch_size x batch_max_len x lstm_hidden_dim                
                     
         # reshape the Variable so that each row contains one token
-        s = s.view(-1, s.shape[2])  # dim: batch_size*batch_max_len x lstm_hidden_dim
+        s = s.contiguous().view(-1, s.shape[2])  # dim: batch_size*batch_max_len x lstm_hidden_dim
                     
         # apply the fully connected layer and obtain the output for each token
         s = self.fc(s)          # dim: batch_size*batch_max_len x num_tags
@@ -63,7 +66,8 @@ def loss_function(outputs, labels):
     mask = (labels >= 0).float()
     
     # the number of tokens is the sum of elements in mask
-    num_tokens = int(torch.sum(mask).data[0])
+    test = torch.sum(mask)
+    num_tokens = int(test.item())
     
     # pick the values corresponding to labels and multiply by mask
     outputs = outputs[range(outputs.shape[0]), labels]*mask
@@ -73,47 +77,55 @@ def loss_function(outputs, labels):
 
 
 def create_index_dicts():
+    word_to_idx = {}
     word_counter = {}
     current_index = 0
-    with open('../nltk/train.txt') as f:
+    with open(TRAIN_PATH) as f:
         for line in f:
             if line.isspace():
                 continue
             else:
                 word = line.split()[0]
                 if word not in word_to_idx:
-                    word_to_idx[word] = current_index
-                    current_index = current_index + 1
+                    #word_to_idx[word] = current_index
+                    #current_index = current_index + 1
                     word_counter[word] = 1
                 else:
                     word_counter[word] = word_counter[word] + 1
-    with open('../nltk/test.txt') as f:
+    with open(TEST_PATH) as f:
         for line in f:
             if line.isspace():
                 continue
             else:
                 word = line.split()[0]
                 if word not in word_to_idx:
-                    word_to_idx[word] = current_index
-                    current_index = current_index + 1
+                    #word_to_idx[word] = current_index
+                    #current_index = current_index + 1
                     word_counter[word] = 1
                 else:
                     word_counter[word] = word_counter[word] + 1
-    with open('../nltk/dev.txt') as f:
+    with open(DEV_PATH) as f:
         for line in f:
             if line.isspace():
                 continue
             else:
                 word = line.split()[0]
                 if word not in word_to_idx:
-                    word_to_idx[word] = current_index
-                    current_index = current_index + 1
+                    #word_to_idx[word] = current_index
+                    #current_index = current_index + 1
                     word_counter[word] = 1
                 else:
                     word_counter[word] = word_counter[word] + 1
-    total_word_to_idx = {word: idx for word, idx in word_to_idx.items() if word_counter[word] > 3}
-    total_word_to_idx['<UNK>'] = current_index
-    total_word_to_idx['<PAD>'] = current_index + 1
+    global total_word_to_idx
+    total_word_to_idx = {}
+    idx = 0
+    for word, count in word_counter.items():
+        if count > 3:
+            total_word_to_idx[word] = idx
+            idx = idx + 1
+    #total_word_to_idx = {word: idx for word, idx in word_to_idx.items() if word_counter[word] > 3}
+    total_word_to_idx['<UNK>'] = idx
+    total_word_to_idx['<PAD>'] = idx + 1
     tag_to_idx['B-PER'] = 0
     tag_to_idx['I-PER'] = 1
     tag_to_idx['B-ORG'] = 2
@@ -141,11 +153,13 @@ def get_tweets_as_idx(path):
             else:
                 tokens = line.split()
                 word = tokens[0]
-                tag = ''
+                tag = tokens[2]
+                '''
                 if len(tokens[2]) == 1:
                     tag = tokens[2]
                 else:
                     tag = tokens[2][2:]
+                '''
                 if word not in total_word_to_idx:
                     sentence.append(total_word_to_idx['<UNK>'])
                 else:
@@ -195,7 +209,8 @@ def train(model, optimizer, loss_fn, data_iterator, params, num_steps):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        loss_avg.update(loss.data[0])
+        #loss_avg.update(loss.data[0])
+        loss_avg.update(loss.item())
         t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
 
 
@@ -207,7 +222,10 @@ def accuracy(outputs, labels):
 
 
 def evaluate(model, loss_fn, data_iterator, metrics, params, num_steps):
-    model.eval()
+    try:
+        model.eval()
+    except:
+        pass
     summ = []
     for _ in range(num_steps):
         data_batch, labels_batch = next(data_iterator)
@@ -217,7 +235,7 @@ def evaluate(model, loss_fn, data_iterator, metrics, params, num_steps):
         output_batch = output_batch.data.cpu().numpy()
         labels_batch = labels_batch.data.cpu().numpy()
         summary_batch = {metric: metrics[metric](output_batch, labels_batch) for metric in metrics}
-        summary_batch['loss'] = loss.data[0]
+        summary_batch['loss'] = loss.item()
         summ.append(summary_batch)
 
     # compute mean of all metrics in summary
@@ -227,11 +245,14 @@ def evaluate(model, loss_fn, data_iterator, metrics, params, num_steps):
     return metrics_mean
 
 
-def predict():
-    tests = get_tweets_as_idx('../nltk/train.txt')
+def predict(params, loss_fn, metrics, model):
+    #model = torch.load('ner_model')
+    tests = get_tweets_as_idx(TEST_PATH)
+    test_size = len(tests[0])
+    num_steps = (test_size + 1) // params['batch_size']
     test_data_iterator = data_iterator(tests[0], tests[1], 16)
-    
-
+    test_metrics = evaluate(model, loss_fn, test_data_iterator, metrics, params, num_steps)
+    return test_metrics
 
 
 def train_and_eval(model, trains, vals, optimizer, loss_fn, metrics, params):
@@ -242,29 +263,38 @@ def train_and_eval(model, trains, vals, optimizer, loss_fn, metrics, params):
     val_labels = vals[1]
     val_size = len(val_data)
     best_val_acc = 0.0
-    for epoch in range(params.num_epochs):
-        logging.info("Epoch {}/{}".format(epoch + 1, params.num_epochs))
-        num_steps = (params.train_size + 1) // params.batch_size
-        train_data_iterator = data_iterator(train_data, train_labels, params.batch_size)
+    for epoch in range(params['num_epochs']):
+        logging.info("Epoch {}/{}".format(epoch + 1, params['num_epochs']))
+        num_steps = (train_size + 1) // params['batch_size']
+        train_data_iterator = data_iterator(train_data, train_labels, params['batch_size'])
         train(model, optimizer, loss_fn, train_data_iterator, params, num_steps)
 
-        num_steps = (params.val_size + 1) // params.batch_size
-        val_data_iterator = data_iterator(val_data, val_labels, params.batch_size)
+        num_steps = (val_size + 1) // params['batch_size']
+        val_data_iterator = data_iterator(val_data, val_labels, params['batch_size'])
         val_metrics = evaluate(model, loss_fn, val_data_iterator, metrics, params, num_steps)
 
         val_acc = val_metrics['accuracy']
         is_best = val_acc >= best_val_acc
 
-                
-def run():
-    create_index_dicts()
-    trains = get_tweets_as_idx('../nltk/train.txt')
-    vals = get_tweets_as_idx('../nltk/dev.txt')
+    torch.save(model.state_dict(), 'ner_model')
 
+                
+def run(metrics):
+    create_index_dicts()
+    trains = get_tweets_as_idx(TRAIN_PATH)
+    vals = get_tweets_as_idx(DEV_PATH)
     vocab_size = len(total_word_to_idx)
     params = {'learning_rate': 1e-3, 'lstm_hidden_dim': 50, 'embedding_dim': 50, \
-        'number_of_tags': 9, 'vocab_size': vocab_size, 'batch_size': 16}
+        'number_of_tags': 9, 'vocab_size': vocab_size, 'batch_size': 16, 'num_epochs': 10}
     model = Net(params)
-    optimizer = optim.Adam(model.parameters(), lr=params['learning_rate'])
-    metrics = {'accuracy': accuracy}
+    optimizer = optim.Adam(model.parameters(), lr=params['learning_rate']) 
     train_and_eval(model, trains, vals, optimizer, loss_function, metrics, params)
+    test_metrics = predict(params, loss_function, metrics, model)
+    print(test_metrics['accuracy'])
+
+if __name__ == '__main__':
+    metrics = {'accuracy': accuracy}
+    run(metrics)
+    #params = {'batch_size': 16}
+    #create_index_dicts()
+    #predict(params, loss_function, metrics)
